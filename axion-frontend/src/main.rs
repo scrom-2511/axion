4d72154cf82647bb8ca5b9ede18243099cc4dea6
scrom <formain337@gmail.com>
2025-12-18 13:38:36 +0000

    add: temp match args and keygen_using_pri_key fn added

--- axion-frontend/src/main.rs
+++ axion-frontend/src/main.rs
@@ -1,12 +1,41 @@
+use std::{env::consts::OS, fs::write};
+
 use clap::Parser;
 
 #[derive(Parser, Debug)]
 enum Args {
-    Keygen {private_key: Option<String>},
+    Keygen { private_key: Option<String> },
     Send { rec_pub_key: String, amount: u32 },
     CheckBalance { pub_key: String },
 }
 
 fn main() {
     let args = Args::parse();
+    match args {
+        Args::Keygen { private_key } => match private_key {
+            Some(pri_key) => {keygen_using_pri_key(pri_key);}
+            None => {}
+        },
+        Args::Send {
+            rec_pub_key,
+            amount,
+        } => {}
+        Args::CheckBalance { pub_key } => {}
+    }
+}
+
+fn keygen_using_pri_key(pri_key: String) {
+    let path = if let Some(home) = dirs::home_dir() {
+        home.join("axion.txt")
+    } else {
+        {
+            eprintln!("I think you are not running an os");
+            return;
+        };
+    };
+
+    match write(path, pri_key) {
+        Ok(_) => {},
+        Err(e)=>{eprintln!("There was an error: \n {}", e)}
+    }
 }
