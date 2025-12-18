ae19925f766b643c65be2fd29bcd71e3e1dbd7f9
scrom <formain337@gmail.com>
2025-12-18 15:27:02 +0000

    add: transaction output struct init

--- axion_core/src/transaction.rs
+++ axion_core/src/transaction.rs
@@ -1,6 +1,13 @@
+use ed25519_dalek::Signature;
+
 pub struct TxInput {
     prev_txid: String,
     output_index: u8,
     signature: Signature,
     pub_key: String
+}
+
+pub struct TxOutput {
+    pub_key: String,
+    amount:u64
 }
