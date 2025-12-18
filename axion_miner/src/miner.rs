023fe2fb1fa1a6f23ffc62805eaba2861f32e1ec
scrom <formain337@gmail.com>
2025-12-18 18:11:12 +0000

    modify: created separate latest block details in miner to make it easy to fetch in frontend

--- axion_miner/src/miner.rs
+++ axion_miner/src/miner.rs
@@ -1,11 +1,14 @@
 use crate::{blockchain::Blockchain, mempool::Mempool, utxo_store::UtxoStore};
 
+pub struct LatestBlockDetails {
+    pub block_height: u128,
+    pub block_hash: String,
+}
 pub struct Miner {
     pub id: String,
     pub mempool: Mempool,
     pub utxo_store: UtxoStore,
-    pub block_height: u128,
-    pub block_hash: String,
     pub reward_address: String,
-    pub blockchain: Blockchain
+    pub blockchain: Blockchain,
+    pub latest_block_details: LatestBlockDetails
 }
