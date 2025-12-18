3ef21d93de0b54ad484569988ab3fbed38c0ba87
scrom <formain337@gmail.com>
2025-12-18 13:07:10 +0000

    add: updated arguments cli

--- axion-frontend/src/main.rs
+++ axion-frontend/src/main.rs
@@ -2,6 +2,7 @@ use clap::Parser;
 
 #[derive(Parser, Debug)]
 enum Args {
+    Keygen {private_key: Option<String>},
     Send { rec_pub_key: String, amount: u32 },
     CheckBalance { pub_key: String },
 }
