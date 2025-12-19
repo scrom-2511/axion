0dacab48fdc526d62a149306dae702a8e7a078d9
scrom <formain337@gmail.com>
2025-12-19 07:00:39 +0000

    add: home dir not found err

--- axion-frontend/src/services/errors_frontend.rs
+++ axion-frontend/src/services/errors_frontend.rs
@@ -2,6 +2,8 @@ use thiserror::{self, Error};
 
 #[derive(Error, Debug)]
 pub enum AxionFrontendError {
+    #[error("Cannot get home path of your system. Make sure you are using a correct OS.")]
+    HomeDirNotFound,
     #[error("Cannot get block headers")]
     BlockHeadersNotFound
 }
